1b7e9ac679c85e03000f4340ddac26b1b2918296
ZipiZipi <veljkoned@gmail.com>
2023-08-11 17:58:18 +0200

    JoystickPipeHole

---
+++ Assets/Scripts/PipeManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeManager : MonoBehaviour
+{
+    [SerializeField] private FloatingJoystick _joystick;
+    [SerializeField] private float _moveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - _moveSpeed * Time.deltaTime);
+        transform.Rotate(0, _joystick.Horizontal * (_moveSpeed/2), 0);
+    }
+}
