ca7fd58eda05e0fdee324aa4f38fa5cb614697ec
ZipiZipi <veljkoned@gmail.com>
2023-08-18 18:47:58 +0200

    FIrst build

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -1,10 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using Unity.UI;
-using UnityEngine.UI;
 
 public class MainMenuController : MonoBehaviour
 {
